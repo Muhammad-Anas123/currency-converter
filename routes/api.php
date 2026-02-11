947aaab9a06b0966f42c69f0363f2973b9d3d6e5
Muhammad Anas <anas.khalil0340@gmail.com>
2026-02-11 14:21:40 +0500

    Changes in APIRouts

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CurrencyController;
 use App\Http\Controllers\ConversionController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-*/
+// Test route to verify API is working
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'API is working!',
+        'laravel_version' => app()->version(),
+        'timestamp' => now()
+    ]);
+});
+
+// Get authenticated user (default Laravel API route)
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 // Currency routes
 Route::prefix('currencies')->group(function () {
